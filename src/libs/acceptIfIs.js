0eb96dc4aba13c1d168f1b441a3f6c5a3e17ebfa
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-06-16 01:14:27 -0700

    delegate the TZ addition logic internally

--- src/libs/acceptIfIs.js
+++ src/libs/acceptIfIs.js
@@ -9,25 +9,29 @@ const acceptIfIs = {
   },
 
   Before(dateInISO) {
-    let date = luxon.DateTime.fromISO(dateInISO);
+    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
 
     return (date.startOf("day") > this._foundDate);
   },
   Between(fromDateInISO, toDateInISO) {
-    let fromDate = luxon.DateTime.fromISO(fromDateInISO);
-    let toDate = luxon.DateTime.fromISO(toDateInISO);
+    let fromDate = luxon.DateTime.fromISO( this._addTheTZ(fromDateInISO));
+    let toDate = luxon.DateTime.fromISO(this._addTheTZ(toDateInISO));
 
     return (this._foundDate.diff(fromDate, ['months', 'days', 'hours']).toObject().days >= 0 && this._foundDate.diff(toDate, ['months', 'days']).toObject().days <= 0);
   },
   After(dateInISO) {
-    let date = luxon.DateTime.fromISO(dateInISO);
+    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
 
     return (this._foundDate.diff(date, ['months', 'days', 'hours']).toObject().days > 0);
   },
   On(dateInISO) {
-    let date = luxon.DateTime.fromISO(dateInISO);
+    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
     console.log(this._foundDate.diff(date, ['months', 'days', 'hours']).toObject())
     return (this._foundDate.diff(date, ['months', 'days', 'hours']).toObject().days === 0);
+  },
+
+  _addTheTZ(dateInISO) {
+    return dateInISO + "T00:00:00.000Z";
   }
 };
 
--- tests/approvementLogic.test.js
+++ tests/approvementLogic.test.js
@@ -11,42 +11,42 @@ const expectItToBe = (foundDate, testLogic, expects) => {
 describe('Want it to be between June 28th and July 5th', function () {
   const testLogic = (acceptIfIs) => {
     return (
-      (acceptIfIs.Between("2022-06-28T00:00:00.000Z", "2022-07-05T00:00:00.000Z")) // If it was in June
+      (acceptIfIs.Between("2022-06-28", "2022-07-05")) // If it was in June
     )
   };
 
   test('should return true if the date is on July 1st', function () {
-    const foundDate = new Date("2022-07-01T00:00:00.000Z");
+    const foundDate = new Date("2022-07-01");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return true if the date is on July 5th', function () {
-    const foundDate = new Date("2022-07-05T00:00:00.000Z");
+    const foundDate = new Date("2022-07-05");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return false if the date is on July 6th', function () {
-    const foundDate = new Date("2022-07-06T00:00:00.000Z");
+    const foundDate = new Date("2022-07-06");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return false if the date is on July 7th', function () {
-    const foundDate = new Date("2022-07-07T00:00:00.000Z");
+    const foundDate = new Date("2022-07-07");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return true if the date is on June 28th', function () {
-    const foundDate = new Date("2022-06-28T00:00:00.000Z");
+    const foundDate = new Date("2022-06-28");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return false the date is on June 27th', function () {
-    const foundDate = new Date("2022-06-27T00:00:00.000Z");
+    const foundDate = new Date("2022-06-27");
 
     expectItToBe(foundDate, testLogic, false);
   });
@@ -55,7 +55,7 @@ describe('Want it to be between June 28th and July 5th', function () {
 describe('Want it to be exactly on June 28th', function () {
   const testLogic = (acceptIfIs) => {
     return (
-      (acceptIfIs.On("2022-06-28T00:00:00.000Z")) // If it was on June 28th
+      (acceptIfIs.On("2022-06-28")) // If it was on June 28th
     )
   };
 
@@ -66,13 +66,13 @@ describe('Want it to be exactly on June 28th', function () {
   });
 
   test('should return false if the date is on June 29th', function () {
-    const foundDate = new Date("2022-06-29T00:00:00.000Z");
+    const foundDate = new Date("2022-06-29");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return false if the date is on June 27th', function () {
-    const foundDate = new Date("2022-06-27T00:00:00.000Z");
+    const foundDate = new Date("2022-06-27");
 
     expectItToBe(foundDate, testLogic, false);
   });
@@ -81,24 +81,24 @@ describe('Want it to be exactly on June 28th', function () {
 describe('Want it to be after on June 28th', function () {
   const testLogic = (acceptIfIs) => {
     return (
-      (acceptIfIs.After("2022-06-28T00:00:00.000Z")) // If it was on June 28th
+      (acceptIfIs.After("2022-06-28")) // If it was on June 28th
     )
   };
 
   test('should return true if the date is on June 29th', function () {
-    const foundDate = new Date("2022-06-29T00:00:00.000Z");
+    const foundDate = new Date("2022-06-29");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return false if the date is on June 28th', function () {
-    const foundDate = new Date("2022-06-28T00:00:00.000Z");
+    const foundDate = new Date("2022-06-28");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return false if the date is on June 27th', function () {
-    const foundDate = new Date("2022-06-27T00:00:00.000Z");
+    const foundDate = new Date("2022-06-27");
 
     expectItToBe(foundDate, testLogic, false);
   });
@@ -107,42 +107,42 @@ describe('Want it to be after on June 28th', function () {
 describe('Want it to be before on June 28th', function () {
   const testLogic = (acceptIfIs) => {
     return (
-      (acceptIfIs.Before("2022-06-28T00:00:00.000Z")) // If it was on June 28th
+      (acceptIfIs.Before("2022-06-28")) // If it was on June 28th
     )
   };
 
   test('should return true if the date is on May', function () {
-    const foundDate = new Date("2022-05-27T00:00:00.000Z");
+    const foundDate = new Date("2022-05-27");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return true if the date is on June 27th', function () {
-    const foundDate = new Date("2022-06-27T00:00:00.000Z");
+    const foundDate = new Date("2022-06-27");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return true if the date is on June 26th', function () {
-    const foundDate = new Date("2022-06-26T00:00:00.000Z");
+    const foundDate = new Date("2022-06-26");
 
     expectItToBe(foundDate, testLogic, true);
   });
 
   test('should return true if the date is on June 28th', function () {
-    const foundDate = new Date("2022-06-28T00:00:00.000Z");
+    const foundDate = new Date("2022-06-28");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return true if the date is on June 29th', function () {
-    const foundDate = new Date("2022-06-29T00:00:00.000Z");
+    const foundDate = new Date("2022-06-29");
 
     expectItToBe(foundDate, testLogic, false);
   });
 
   test('should return true if the date is on June 30th', function () {
-    const foundDate = new Date("2022-06-30T00:00:00.000Z");
+    const foundDate = new Date("2022-06-30");
 
     expectItToBe(foundDate, testLogic, false);
   });
