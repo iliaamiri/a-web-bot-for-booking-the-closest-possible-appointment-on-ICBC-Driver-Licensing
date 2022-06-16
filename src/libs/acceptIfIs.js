60636d11b630b574e55cdc6918585a29eaeacf9a
Ilia Abedianamiri <37903573+iliaamiri@users.noreply.github.com>
2022-06-16 13:55:56 -0700

    dry

--- src/libs/acceptIfIs.js
+++ src/libs/acceptIfIs.js
@@ -9,27 +9,30 @@ const acceptIfIs = {
   },
 
   Before(dateInISO) {
-    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
+    let date = this._getLuxonDateFromISO(dateInISO);
 
     return (date.startOf("day") > this._foundDate);
   },
   Between(fromDateInISO, toDateInISO) {
-    let fromDate = luxon.DateTime.fromISO( this._addTheTZ(fromDateInISO));
-    let toDate = luxon.DateTime.fromISO(this._addTheTZ(toDateInISO));
+    let fromDate = this._getLuxonDateFromISO(fromDateInISO);
+    let toDate = this._getLuxonDateFromISO(toDateInISO);
 
     return (this._foundDate.diff(fromDate, ['months', 'days', 'hours']).toObject().days >= 0 && this._foundDate.diff(toDate, ['months', 'days']).toObject().days <= 0);
   },
   After(dateInISO) {
-    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
+    let date = this._getLuxonDateFromISO(dateInISO);
 
     return (this._foundDate.diff(date, ['months', 'days', 'hours']).toObject().days > 0);
   },
   On(dateInISO) {
-    let date = luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
+    let date = this._getLuxonDateFromISO(dateInISO);
     console.log(this._foundDate.diff(date, ['months', 'days', 'hours']).toObject())
     return (this._foundDate.diff(date, ['months', 'days', 'hours']).toObject().days === 0);
   },
 
+  _getLuxonDateFromISO(dateInISO) {
+    return luxon.DateTime.fromISO(this._addTheTZ(dateInISO));
+  },
   _addTheTZ(dateInISO) {
     return dateInISO + "T00:00:00.000Z";
   }
